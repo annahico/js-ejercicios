156c9c1edaee19041a5ad40db427cf47d29bedb6
annahico <annahico@gmail.com>
2024-04-07 13:23:56 +0200

    empezando ejercicio 3 JS

---
+++ js/Ejercicio-03.js
@@ -0,0 +1,11 @@
+/*
+indice validar que no sea menor a cero y que el elemento exista
+en el array
+*/
+
+function getByIdx(arr, idx) {
+
+}
+
+let resultado = getByIdx([1, 2], 1);
+console.log(resultado);
