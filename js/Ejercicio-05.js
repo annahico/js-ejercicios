5fc874cd0a0bd008ec5ede80967ee82bac675ccf
annahico <annahico@gmail.com>
2024-04-07 13:40:23 +0200

    empezando ejercicio 5 JS

---
+++ js/Ejercicio-05.js
@@ -0,0 +1,13 @@
+/*
+Crear algoritmo que devuelva número
+menor y mayor de un array
+*/
+
+let array = [2, 5, 7, 15, -5, -100, 55];
+
+function getMenorMayor(arr) {
+
+}
+
+let numeros = getMenorMayor(array)
+console.log(numeros);
