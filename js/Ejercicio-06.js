7b9058713064665692698678d18c2c373ae8cc42
annahico <annahico@gmail.com>
2024-04-07 13:51:56 +0200

    preparando ejercicio 6 JS

---
+++ js/Ejercicio-06.js
@@ -0,0 +1,13 @@
+/*
+Crear algoritmo que devuelva cantidad
+de núm,eros positivos de un array
+*/
+
+let array = [2, 5, 7, 15, -5, -100, 55];
+
+function cuantosPositivos(arr) {
+
+}
+    // function cuantosPositivos(arr: any): void
+let resultado = cuantosPositivos(array);
+console.log(resultado);
