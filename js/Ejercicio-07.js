539d20dd79b01f88422c08bf2e4e00a32a2b3687
annahico <annahico@gmail.com>
2024-04-21 10:16:03 +0200

    finalizando ejercicio 7 JS

--- js/Ejercicio-07.js
+++ js/Ejercicio-07.js
@@ -4,9 +4,9 @@ el precio del producto
 más impuesto
 */
 
-function precioCompleto(precio, impuesto){
-
+function precioCompleto(precio, impuesto) {
+     return precio + precio * impuesto; //se puede redondear pero aún no se ha visto la teoría
 }
 
-let resultado = precioCompleto(19.90, 0.15);
+let resultado = precioCompleto(19.90, 0.15); //precio + impuesto
 console.log(resultado);
