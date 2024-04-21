ce65a0341a4768b1daa4de7205d1f5c2c73f1a69
annahico <annahico@gmail.com>
2024-04-21 10:53:45 +0200

    empezando ejercicio 10 JS

---
+++ js/Ejercicio-10.js
@@ -0,0 +1,13 @@
+/*
+Crear array de longitud N, y que sus elementos sean
+números de 1 hasta N
+*/
+
+let longitud = 7;
+
+function crearArray(n) {
+
+}
+let resultado = crearArray(longitud);
+
+console.log(resultado);
