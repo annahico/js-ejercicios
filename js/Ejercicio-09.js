7716888645f0c67afc4622c1e7ca79e3df377922
annahico <annahico@gmail.com>
2024-04-21 10:48:47 +0200

    terminando ejercicio 9 JS

--- js/Ejercicio-09.js
+++ js/Ejercicio-09.js
@@ -3,7 +3,7 @@ Crear algorítmo que devuelva un
 array de objetos en base a pares
 */
 
-let pares = [
+let pairs = [
     [1, { name: "Anna" }],
     [2, { name: "Carlos" }],
     [3, { name: "Naruto" }],
@@ -21,8 +21,14 @@ let array = [{
 }];
 
 function toCollection(arr) {
-
+    let collection = [];
+    for (idx in arr) {
+        let elemento = arr[idx];
+        collection[idx] = elemento[1];
+        collection[idx].id = elemento[0];
+    }
+    return collection;
 }
 
-let resultado = toCollection(array);
+let resultado = toCollection(pairs);
 console.log(resultado);
