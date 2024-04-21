c702e5052c4b016f382e4d1934378d20632c7fbf
annahico <annahico@gmail.com>
2024-04-21 10:58:41 +0200

    terminando ejercicio 10 y los ejercicios JS de Hola Mundo

--- js/Ejercicio-10.js
+++ js/Ejercicio-10.js
@@ -6,7 +6,14 @@ números de 1 hasta N
 let longitud = 7;
 
 function crearArray(n) {
-
+    if (n <= 0) {
+        return [];
+    }
+    let arr =[];
+    for (let i = 0; i < n; i++) {
+        arr[i] =i + 1;
+    }
+    return arr;
 }
 let resultado = crearArray(longitud);
 
